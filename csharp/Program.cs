06e207844e96454c58f1d99ddc31492176920bbf
jrmartinez2100 <martinezjuniormanuel175@gmail.com>
2022-01-29 09:49:17 -0600

    archivo nuevo

---
+++ csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, suma, producto;
+            String Linea;
+            Console.WriteLine("Ingrese el primer valor :");
+            Linea = Console.ReadLine();
+            num1 = int.Parse(Linea);
+            Console.WriteLine("Ingrese el segundo valor :");
+            Linea = Console.ReadLine();
+            num2 = int.Parse(Linea);
+            suma = num1 + num2;
+            producto = num1 * num2;
+            Console.WriteLine("La suma de los dos valores es :");
+            Console.WriteLine(suma);
+            Console.WriteLine("El producto de los dos valores es :");
+            Console.WriteLine(producto);
+        }
+    }
+}
